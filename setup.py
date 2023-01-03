aaea6cc34cc6fec6d28bb90b84fdca929b6feb14
Jeffrey Quesnelle <jq@jeffq.com>
2023-01-03 14:37:05 -0500

    PyPI publication

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     packages=["transformers_openai_api"],
     version='1.0.0',
     description='An OpenAI Completions API compatible server for NLP transformers models',
-    author='Jeffrey Quesnelle <jq@jeffq.com>',
+    author='Jeffrey Quesnelle',
+    author_email='jq@jeffq.com',
+    url='https://github.com/jquesnelle/transformers-openai-api/',
     license='MIT',
     install_requires=[
         'transformers',
@@ -17,5 +19,10 @@ setup(
         'console_scripts': [
             'transformers-openai-api = transformers_openai_api.__main__:main'
         ]
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
